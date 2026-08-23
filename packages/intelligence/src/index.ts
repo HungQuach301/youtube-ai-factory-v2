dd111ea263b8001417767c89633c9ad76627a501
HungQuach301 <92220096+HungQuach301@users.noreply.github.com>
2026-08-23 20:59:49 +0700

    WP-17: add packages/intelligence/src/index.ts

---
+++ packages/intelligence/src/index.ts
@@ -0,0 +1,12 @@
+export { isFresh, lintAudienceJob } from './audience.js'
+export type { SignalKind } from './audience.js'
+export {
+  beatSequenceDiff,
+  cosineSimilarity,
+  differentiationScore,
+  measureAntiCopy,
+  ngramJaccard,
+  phashHamming,
+  sharedNgramCount,
+} from './anti-copy.js'
+export type { AntiCopyResult } from './anti-copy.js'
