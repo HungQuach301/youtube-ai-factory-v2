b8c4049e684dfc1658f9512fdabc7825a1263bd5
HungQuach301 <92220096+HungQuach301@users.noreply.github.com>
2026-08-23 20:50:04 +0700

    WP-16: add packages/truth-layer/src/index.ts

---
+++ packages/truth-layer/src/index.ts
@@ -0,0 +1,14 @@
+export { lintAdvice } from './advice.js'
+export { parseNumbers } from './numeric.js'
+export { defineTerminology, validateClaimEvidence } from './truth.js'
+export {
+  ClaimCriticalitySchema,
+  ClaimRecordSchema,
+  ClaimSourceSchema,
+  ClaimTypeSchema,
+  SourceRecordSchema,
+  SourceRoleSchema,
+  SourceTierSchema,
+  TerminologySchema,
+} from './types.js'
+export type { AdviceFinding, ClaimRecord, ClaimSource, ParsedNumber, SourceRecord, Terminology } from './types.js'
